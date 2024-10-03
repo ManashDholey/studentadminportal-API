990886fe8202cdc91b54aead5dc8d5dba560c831
ManashDholey <chottu.dholey52@gmail.com>
2024-10-04 03:07:16 +0530

    Add cross setting

---
+++ studentadminportal-API/CorsSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace studentadminportal_API
+{
+    public class CorsSettings
+    {
+        public string[] AllowedOrigins { get; set; }
+    }
+}
--- studentadminportal-API/Program.cs
+++ studentadminportal-API/Program.cs
@@ -6,11 +6,13 @@ using FluentValidation.AspNetCore;
 using Infrastructure.Data.Repositories;
 using Infrastructure.Data.Unit;
 using Infrastructure.Services;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.OpenApi.Models;
+using studentadminportal_API;
 using studentadminportal_API.DataModels;
 using studentadminportal_API.Extensions;
 using studentadminportal_API.FileServices;
@@ -19,12 +21,27 @@ using studentadminportal_API.Middleware;
 using studentadminportal_API.Profile;
 
 var builder = WebApplication.CreateBuilder(args);
+var resourcePath = Path.Combine(builder.Environment.ContentRootPath, builder.Configuration.GetValue<string>("ResourcePath"));
 
+// Ensure the directory exists
+if (!Directory.Exists(resourcePath))
+{
+    Directory.CreateDirectory(resourcePath);
+}
+builder.Services.AddSingleton<IFileProvider>(new PhysicalFileProvider(resourcePath));
+builder.Services.AddDataProtection()
+    .PersistKeysToFileSystem(new DirectoryInfo(@"h:\root\home\kumardholey-001\www\site1\keys"))
+    .SetApplicationName("kumardholey-001");
+builder.Services.AddHttpsRedirection(options =>
+{
+    options.HttpsPort = 443; // The default HTTPS port
+});
+var corsSettings = builder.Configuration.GetSection("CorsSettings").Get<CorsSettings>();
 builder.Services.AddCors((options) =>
 {
     options.AddPolicy("angularApplication", (builder) =>
     {
-        builder.WithOrigins("http://localhost:4200")
+        builder.WithOrigins(corsSettings?.AllowedOrigins!)
         .AllowAnyHeader()
         .WithMethods("GET", "POST", "PUT", "DELETE")
         .WithExposedHeaders("*");
@@ -35,7 +52,11 @@ builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
-
+//builder.Services.Configure<CookiePolicyOptions>(options =>
+//{
+//    options.MinimumSameSitePolicy = SameSiteMode.None;
+//    options.Secure = CookieSecurePolicy.Always;
+//});
 //builder.Services.AddSwaggerGen(c =>
 //{
 //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "StudentAdminPortal.API", Version = "v1" });
@@ -43,6 +64,7 @@ builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
 
 
 var app = builder.Build();
+app.UseCors("angularApplication");
 app.UseMiddleware<JwtMiddleware>();
 app.UseMiddleware<ExceptionMiddleware>();
 // Configure the HTTP request pipeline.
@@ -52,6 +74,12 @@ if (app.Environment.IsDevelopment())
     //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "StudentAdminPortal.API v1"));
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 
 app.UseHttpsRedirection();
 
@@ -63,7 +91,7 @@ app.UseStaticFiles(new StaticFileOptions
 
 app.UseRouting();
 
-app.UseCors("angularApplication");
+
 
 app.UseAuthorization();
 
